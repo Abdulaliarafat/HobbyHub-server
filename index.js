8119f6f349bc7db4fef72ca4661d703d02a141e0
abdulaliarafat <abdulaliarafat@gmail.com>
2025-06-27 07:21:29 +0600

    modify leastest group

--- index.js
+++ index.js
@@ -21,40 +21,41 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
+    await client.connect();
+// All group
     const groupCollection=client.db('groupDB').collection('group')
     app.get('/group/all',async(req,res)=>{ 
       const result= await groupCollection.find().toArray();
       res.send(result)
     })
-
+    // letest group
     app.get('/group/latest',async(req,res)=>{
-      const today=new Date();
-      const start=new Date(today.setHours(0,0,0,0));
-      const end=new Date (today.setHours(23,59,59,999));
       const sortField={date:- 1}
-      const result= await groupCollection.find({date:{$gte: start,$lte:end}}).sort(sortField).limit(6).toArray()
+      const result= await groupCollection.find().sort(sortField).limit(6).toArray()
       res.send(result)
     })
+    // My group
    app.get('/group/email/:email',async(req,res)=>{
     const email=req.params.email;
     const query={email:email};
     const result=await groupCollection.find(query).toArray();
     res.send(result)
    })
+     //  group details
     app.get('/group/id/:id',async(req,res)=>{
       const id=req.params.id;
       const query={_id:new ObjectId(id)}
       const result=await groupCollection.findOne(query);
       res.send(result)
     })
+    //  post in DB
     app.post('/group',async(req,res)=>{
       const groupData=req.body;
       console.log(groupData)
       const result=await groupCollection.insertOne(groupData);
       res.send(result)
     })
+    // update group
     app.put('/group/id/:id',async(req,res)=>{
       const id=req.params.id;
       const filter={_id:new ObjectId(id)};
@@ -66,6 +67,7 @@ async function run() {
       const result=await groupCollection.updateOne(filter,updateGroup,options)
       res.send(result)
     })
+    // detete from DB
     app.delete('/group/id/:id',async(req,res)=>{
       const id=req.params.id;
       const query={_id:new ObjectId(id)};
@@ -74,8 +76,8 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
